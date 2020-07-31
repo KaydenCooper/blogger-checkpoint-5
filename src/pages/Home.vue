2581f55dd3d725ac0e795a05cb2d058a750764e7
Kayden Cooper <kayden_cooper@yahoo.com>
2020-07-31 12:33:25 -0600

    initialized logging to console

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home row">
+    <div class="col-4"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  computed: {
+    blogs() {
+      return this.$store.state.blogs;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getAllBlogs");
+  },
+  methods: {},
+  components: {},
+};
+</script>
